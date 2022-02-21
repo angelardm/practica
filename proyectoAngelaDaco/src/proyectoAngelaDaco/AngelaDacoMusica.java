e529ebfd800b0bed860bd7562e0ffc33482c7f16
Angela <Angela@DESKTOP-HHCAT4U>
2022-02-21 02:05:47 +0100

    Este es mi primer commit de get

---
+++ proyectoAngelaDaco/src/proyectoAngelaDaco/AngelaDacoMusica.java
@@ -0,0 +1,17 @@
+package proyectoAngelaDaco;
+
+public class AngelaDacoMusica {
+public String nombreHobby;
+public String cantante;
+public String cancion;
+
+public AngelaDacoMusica(String nombreHobby, String cantante, String cancion) {
+	this.nombreHobby=nombreHobby;
+	this.cantante=cantante;
+	this.cancion=cancion;
+}
+public String getNombreHobby() {
+	return nombreHobby;
+}
+}
+
